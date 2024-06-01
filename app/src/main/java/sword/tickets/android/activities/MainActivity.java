b4db767a4f7c91f865a67d1b5a279eda36eba806
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2024-06-01 15:27:27 +0200

    Add project with empty activity

---
+++ app/src/main/java/sword/tickets/android/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package sword.tickets.android.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import sword.tickets.android.R;
+
+public final class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
